67822b44d28f76602626d89fa1a3223e7d84b126
HubertLoetsch <hubert.loetsch@hotmail.com>
2021-03-08 19:53:22 +0100

    add switch in main add a new line (sout)

---
+++ src/com/company/Switch.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int note = sc.nextInt();
+
+        switch (note) {
+            case 1 -> System.out.println("sehr gut");
+            case 2 -> System.out.println("gut");
+            case 3 -> System.out.println("befriedigend");
+            case 4 -> System.out.println("genügend");
+            case 5 -> System.out.println("nicht genügend");
+            default -> System.out.println("Falsche Eingabe!");
+        }
+    }
+}
+
