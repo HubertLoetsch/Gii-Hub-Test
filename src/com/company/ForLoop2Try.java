bc15f3b18615da1478a628b9fedfb408e6b15c4a
HubertLoetsch <hubert.loetsch@hotmail.com>
2021-03-08 21:05:50 +0100

    ForLoopcommands modified (sout sum to Summe)
    Add a new Project ForLoop2Try

--- src/com/company/ForLoop2Try.java
+++ src/com/company/ForLoop2Try.java
@@ -1,9 +1,9 @@
 package com.company;
 
-public class ForLoopcommands {
+public class ForLoop2Try {
     public static void main(String[] args) {
 
-        int a = 100, b = 0;
+        int a = 10, b = 0;
 
         for(int i = 1; i <= a; ++i)
         {
@@ -14,3 +14,14 @@ public class ForLoopcommands {
         System.out.println("Summe ist  = " + b);
     }
 }
+
+
+
+
+/*
+
+Zähle die geraden Ziffern zwischen 1 und 1000 zusammen -
+Tip: Starte den Loop bei 2 und erhöhe den Zählindex jeweils um 2.
+
+ */
+
