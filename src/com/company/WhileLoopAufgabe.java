d42cdee380f92ce1a9f4c19aacd823dfc737f2cf
HubertLoetsch <hubert.loetsch@hotmail.com>
2021-03-08 21:08:00 +0100

    ForLoopcommands modified (sout sum to Summe)
    Add a new Project ForLoop2Try

---
+++ src/com/company/WhileLoopAufgabe.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Random;
+
+public class WhileLoopAufgabe {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+
+    }
+}
+
+//TODO randomNumber 10 to 30
+
+/*
+Erstelle ein Programm, dass Zufallszahlen zwischen 10 und 30 generiert.
+
+Das Programm soll die Zufallszahlen zusammenzählen.
+
+Sobald die Zahl 15 oder die Zahl 25 kommt, wird das Programm beendet und die Summe der vorherigen Zufallszahlen ausgegeben!
+
+ */
