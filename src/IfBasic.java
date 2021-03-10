e0d7899925fb0bfe95b73a2e4ef0b67b5ffd8f33
HubertLoetsch <hubert.loetsch@hotmail.com>
2021-03-10 20:16:06 +0100

    ForLoop2try finish

---
+++ src/IfBasic.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class IfBasic {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber1 = random.nextInt(100);
+        int randomNumber2 = random.nextInt(100);
+
+        if (randomNumber1 > 50 || randomNumber2 < 50) {
+            System.out.println("Die Zahl 1 ist kleiner als Zahl 2 und mini ");
+
+        }
+        if (randomNumber1 > 30 && randomNumber2 < 30) {
+            System.out.println("Eine der beiden ist kleiner als 3o");
+
+        }
+        if (randomNumber1 > 50 != randomNumber2 < 50) {
+            System.out.println("Erste Zahl klein, zweite kein 50iger");
+        }
+
+    }
+}
